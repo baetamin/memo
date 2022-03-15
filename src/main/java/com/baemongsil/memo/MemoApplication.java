6b8a5e5746b6e640e57cd84b6576952ba184a604
배준희 22/02/15 첫 파일 올리기 <>
2022-03-15 17:33:27 +0900

    3/15 memo

---
+++ src/main/java/com/baemongsil/memo/MemoApplication.java
@@ -0,0 +1,16 @@
+package com.baemongsil.memo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/baemongsil/memo/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.baemongsil.memo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "hello world!";
+	}
+}
